cfa672a0ad4bf3ff588e893a643aceb96d97dd21
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-10-03 19:49:26 +1000

    ThemeContextProv
    Created Themecpntext provider file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeContext } from './contexts/ThemeContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
     <App />
   </StrictMode>,
 )
