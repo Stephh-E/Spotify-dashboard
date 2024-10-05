525ff7b7fe7ef4cad819d933936bcebd4ffa7ba3
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-10-05 10:37:43 +1000

    Update main.jsx
    -

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeContextProvider } from './contexts/ThemeContextProvider.jsx'
-import {SpotifyAuthProvider} from './contexts/SpotifyAuthProvider.jsx'
+import { SpotifyAuthProvider } from './contexts/SpotifyAuthProvider.jsx'
+import { SpotifyProfileProvider } from './contexts/SpotifyProfileProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeContextProvider>
       <SpotifyAuthProvider>
-
-      <App />
+        <SpotifyProfileProvider>
+          <App />
+        </SpotifyProfileProvider>
       </SpotifyAuthProvider>
-    </ThemeContextProvider>
+
+    </ThemeContextProvider>    
   </StrictMode>,
 )
